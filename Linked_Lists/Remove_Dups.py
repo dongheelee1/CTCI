d327ceb52e6c23b470f804fe6d7f9474741d95e0
donghizzle <dongheec.lee@gmail.com>
2019-05-26 17:48:55 -0400

    Update Remove_Dups.py

--- Linked_Lists/Remove_Dups.py
+++ Linked_Lists/Remove_Dups.py
@@ -1,4 +1,45 @@
 '''
 Remove duplicates from an unsorted linked list. 
 '''
+'''
+83. Remove Duplicates from Sorted List
 
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example 1:
+
+Input: 1->1->2
+Output: 1->2
+Example 2:
+
+Input: 1->1->2->3->3
+Output: 1->2->3
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        '''
+        list is sorted 
+        
+        simple -- just compare next element's value with current and manipulate the next pointer
+        '''
+        
+        if head is None: 
+            return head 
+        
+        curr = head 
+        #[1, 1, 2, 3, 3]
+        while curr.next is not None and curr is not None: 
+            
+            if curr.val == curr.next.val: 
+                curr.next = curr.next.next 
+            else: 
+                curr = curr.next 
+            
+        return head
+                
