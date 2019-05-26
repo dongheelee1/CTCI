15d0ed21aa21696d26f59d8a533881f4370d2269
donghizzle <dongheec.lee@gmail.com>
2019-05-26 14:03:43 -0400

    Update Delete_Middle_Node.py

--- Linked_Lists/Delete_Middle_Node.py
+++ Linked_Lists/Delete_Middle_Node.py
@@ -1,4 +1,13 @@
 '''
-Delete a node in thee middle (i.e. any node but the first and last node, not necessarily the exact middle of a singly linked list, given only access to that node. 
+Delete a node in the middle (i.e. any node but the first and last node, not necessarily the exact middle of a singly linked list, given only access to that node. 
 '''
 
+class Solution:
+    def deleteNode(self, node):
+        """ 
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        
+        node.val = node.next.val #replace value of current node to be value of current node's next 
+        node.next= node.next.next #current node's next is set to current node's next.next
