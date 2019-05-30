d4ab207d43c954d47b04b7bfada1e00fc0f2d9b5
donghizzle <dongheec.lee@gmail.com>
2019-05-29 23:47:26 -0400

    Update check_if_bst.py

--- Trees_and_Graphs/check_if_bst.py
+++ Trees_and_Graphs/check_if_bst.py
@@ -57,8 +57,21 @@ class Solution(object):
       1   6
          / \
         3   9
-
+    Correct Idea: 
     
+    At every level of the function call (going top down), check that the root's value is within the bounds. 
+    
+    If not, return False 
+    
+    Then, go deeper down the tree by recursively calling helper function on...
+        root.left, passing in upper = root.val, lower stays the same 
+        and then, 
+        root.right, passing in upper stays the same, lower = root.val 
+        
+     If any of these calls returns False / not True, return False 
+
+     Otherwise, return True. 
+
     '''
     import sys 
     def isValidBST(self, root):
@@ -72,13 +85,13 @@ class Solution(object):
             if root is None: 
                 return True 
 
-            if root.val <= lower or root.val >= upper: 
+            if root.val <= lower or root.val >= upper: #when root value is out of bounds, then return False 
                 return False
             
-            if not isValidBST_helper(root.left, lower, root.val): 
+            if not isValidBST_helper(root.left, lower, root.val): #check if left branch is a valid BST, keeping lower bound the same and setting upper bound as root.val
                 return False 
             
-            if not isValidBST_helper(root.right, root.val, upper): 
+            if not isValidBST_helper(root.right, root.val, upper): #check if right branch is a valid BST, keeping upper bound the same and setting lower bound as the root.val
                 return False 
             
             return True 
