15051df04d36a72b9126c89922f0336baa05ed7c
donghizzle <dongheec.lee@gmail.com>
2019-05-30 23:24:05 -0400

    Update 1_isUnique.py

--- strings_matrices/1_isUnique.py
+++ strings_matrices/1_isUnique.py
@@ -1,10 +1,22 @@
 '''
 Algorithm to determine if a sring has all unique characters. What if you cannot use additional data structures?
 '''
+'''
+IDEA: 
 
 sort the string 
 
-check if current char is the same as the char after 
+check to see if current char is the same as the char after 
 
-if so, return False 
-if not, increment the pointer across string
+if so, return False (string not unique)
+if not, continue in for loop 
+
+Note: string is converted to arr 
+'''
+
+def is_unique(arr): 
+  for i in range(0, len(arr) - 1): 
+    if arr[i] == arr[i+1]: 
+      return False
+    else: 
+      return True 
